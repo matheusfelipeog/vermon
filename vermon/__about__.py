4f8b45b7f4538d5e2b2aad21e722b16c0158f715
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2021-04-01 13:13:51 -0300

    Add vermon metadata

---
+++ vermon/__about__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Vermon Metadata."""
+
+__author__ = 'Matheus Felipe'
+__email__ = 'matheusfelipeog@protonmail.com'
+__author_github__ = 'https://github.com/matheusfelipeog'
+__project_github__ = 'https://github.com/matheusfelipeog/vermon'
