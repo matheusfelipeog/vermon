07848d609b3c1aeb57c9fd39e3e0eed898459810
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2021-04-06 17:53:25 -0300

    Create exception to unsupported version pattern

---
+++ vermon/exceptions.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+"""All exceptions used in vermon package."""
+
+class UnsupportedVersionPattern(Exception):
+    """Used to raise a exception for the unsupported version pattern."""
