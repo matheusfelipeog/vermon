ad2608a11dd9d0d057f2ad06187a364b3a92793b
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2021-04-07 14:49:43 -0300

    Add imports in vermon.__init__.py

--- vermon/__init__.py
+++ vermon/__init__.py
@@ -1,1 +1,21 @@
 # -*- coding: utf-8 -*-
+
+"""
+Vermon Package
+--------------
+
+This package is used to check for a newer version of 
+a package and notify the user about it.
+"""
+
+from vermon.__about__ import __version__
+from vermon.__about__ import __author__
+from vermon.__about__ import __email__
+from vermon.__about__ import __author_github__
+from vermon.__about__ import __project_github__
+
+from vermon.core import Vermon
+
+from vermon.exceptions import UnsupportedVersionPattern
+from vermon.exceptions import PackageNotFound
+from vermon.exceptions import PackageNotFoundOnPypi
