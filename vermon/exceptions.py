bbc67583a2c08e86c7184fd8b331d07726ee68e9
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2021-04-07 14:26:37 -0300

    Add two new exception in case of package not found

--- vermon/exceptions.py
+++ vermon/exceptions.py
@@ -2,5 +2,14 @@
 
 """All exceptions used in vermon package."""
 
+
 class UnsupportedVersionPattern(Exception):
     """Used to raise a exception for the unsupported version pattern."""
+
+
+class PackageNotFound(Exception):
+    """Used to raise a exception when package not found on platform."""
+
+
+class PackageNotFoundOnPypi(PackageNotFound):
+    """Used to raise a exception when package not found on Pypi."""
